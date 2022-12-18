b34f73d96aae6af008e848ef5e0e407d5cabed0a
RakeMeUp <bencehetes@gmail.com>
2022-12-18 01:01:29 +0100

    better naming

--- app/form/Table.tsx
+++ app/form/Table.tsx
@@ -2,48 +2,50 @@ import { MdDeleteForever } from "react-icons/md";
 import { Entry } from "../../Interfaces/Entry";
 
 type Props = {
-    obj: Entry[];
+    entryArray: Entry[];
     handleRemove: (start: Date) => void;
 };
 
-function Table({ obj, handleRemove }: Props) {
+function Table({ entryArray, handleRemove }: Props) {
     return (
         <table className="bg-themedarkgray text-themelightgray relative text-sm md:text-base lg:text-lg min-h-[300px]">
             <thead className=" bg-themedarkgray text-themelightgray sticky top-0 left-0 right-0">
-                <th>Project</th>
-                <th>Area</th>
-                <th>LAS</th>
-                <th>Operator</th>
-                <th>Start</th>
-                <th>End</th>
-                <th></th>
+                <tr>
+                    <th>Project</th>
+                    <th>Area</th>
+                    <th>LAS</th>
+                    <th>Operator</th>
+                    <th>Start</th>
+                    <th>End</th>
+                    <th></th>
+                </tr>
             </thead>
             <tbody>
-                {obj.map((e) => {
+                {entryArray.map((entry) => {
                     return (
-                        <tr key={e.LAS} className="text-center odd:bg-black">
-                            {Object.values(e).map((e, i) => {
-                                if (e instanceof Date)
+                        <tr key={entry.data.LAS} className="text-center odd:bg-black">
+                            {Object.values(entry.data).map((value, i) => {
+                                if (value instanceof Date)
                                     return (
                                         <td className="px-2 lg:px-8 max-w-[120px]" key={i}>
-                                            {e.toLocaleTimeString().slice(0, -3)}
+                                            {value.toLocaleTimeString().slice(0, -3)}
                                         </td>
                                     );
                                 if (i === 2)
                                     return (
                                         <td className="px-2 lg:px-8" key={i}>
-                                            {e.slice(0, -4)}
+                                            {value.slice(0, -4)}
                                         </td>
                                     );
                                 return (
                                     <td className="px-2 lg:px-8" key={i}>
-                                        {e}
+                                        {value}
                                     </td>
                                 );
                             })}
                             <td className="w-14 px-2 ">
                                 <button
-                                    onClick={() => handleRemove(e.Start)}
+                                    onClick={() => handleRemove(entry.data.Start)}
                                     className="rounded-full w-full h-full"
                                 >
                                     <MdDeleteForever className="text-themered w-full h-full p-1" />
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -4,22 +4,27 @@ import { Entry } from "../../Interfaces/Entry";
 import clipboardToEntry from "../../utils/clipboardToEntry";
 import Table from "./Table";
 import { poster } from "../../api/poster";
+import { useSession } from "next-auth/react";
+import { showError } from "../../utils/showError";
 import { toast } from "react-toastify";
 
 type Props = {};
 
 function Form({}: Props) {
-    const [obj, setObj] = useState<Entry[]>([]);
+    const { data: session } = useSession();
+
+    const [entryArray, setEntryArray] = useState<Entry[]>([]);
 
     useEffect(() => {
         const handlePasteAnywhere = (event: any) => {
             let data = event.clipboardData.getData("text");
             try {
-                let object: Entry[] = clipboardToEntry(data);
+                let object: Entry[] = clipboardToEntry(data, session?.user.idToken!);
                 console.log(object);
 
-                setObj(object);
+                setEntryArray(object);
             } catch (e) {
+                showError();
                 console.log(e);
             }
         };
@@ -29,51 +34,53 @@ function Form({}: Props) {
         return () => {
             window.removeEventListener("paste", handlePasteAnywhere);
         };
-    }, []);
+    }, [session]);
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        const err = await poster("POST", "api/entries", obj);
+        const err = await poster("POST", "api/entries", entryArray);
         if (err) {
-            toast("Error happened while sending, Try Again", {
-                hideProgressBar: true,
-                autoClose: 5000,
-                type: "error",
-            });
+            showError("Error happened while sending, Try Again");
         } else {
             toast("Success", {
                 type: "success",
             });
-            setObj([]);
+            setEntryArray([]);
         }
     };
 
     const handleRemove = (start: Date) => {
-        setObj((prev) => prev.filter((e) => e.Start !== start));
+        setEntryArray((prev) => prev.filter(({ data }) => data.Start !== start));
     };
 
     return (
         /* inset css bcuz nextjs puts boundaries in html that fucks with style */
         <div className="absolute inset-0">
             <form className="bg-themelightgray btn-shadow flex flex-col m-10 rounded-3xl h-2/3">
-                <h2 className="text-center py-3 text-[30px] font-bold">Excel Paste</h2>
-                <div className="max-h-[80%] rounded-3xl overflow-scroll scrollbar-hide w-fit mx-auto">
-                    {obj.length ? (
-                        <Table handleRemove={handleRemove} obj={obj} />
-                    ) : (
-                        <div className="p-28 text-xl md:p-52 bg-themedarkgray text-themelightgray">
-                            Paste Here
+                {session ? (
+                    <>
+                        <h2 className="text-center py-3 text-[30px] font-bold">Excel Paste</h2>
+                        <div className="max-h-[80%] rounded-3xl overflow-scroll scrollbar-hide w-fit mx-auto">
+                            {entryArray.length ? (
+                                <Table handleRemove={handleRemove} entryArray={entryArray} />
+                            ) : (
+                                <div className="p-28 text-xl md:p-52 bg-themedarkgray text-themelightgray">
+                                    Paste Here
+                                </div>
+                            )}
                         </div>
-                    )}
-                </div>
 
-                <button
-                    disabled={obj.length === 0}
-                    onClick={handleSubmit}
-                    className="border-themedarkgray w-fit mx-auto my-10 border-2 px-4 py-1 rounded-full hover:bg-themedarkgray hover:text-themelightgray active:bg-themelightgray active:text-themedarkgray disabled:hidden"
-                >
-                    Send Data
-                </button>
+                        <button
+                            disabled={entryArray.length === 0}
+                            onClick={handleSubmit}
+                            className="border-themedarkgray w-fit mx-auto my-10 border-2 px-4 py-1 rounded-full hover:bg-themedarkgray hover:text-themelightgray active:bg-themelightgray active:text-themedarkgray disabled:hidden"
+                        >
+                            Send Data
+                        </button>
+                    </>
+                ) : (
+                    <div>Log in to user</div>
+                )}
             </form>
         </div>
     );
