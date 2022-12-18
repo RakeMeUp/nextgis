cb1cc1da424d077bbca2bcc5b735310eef0c8a83
RakeMeUp <bencehetes@gmail.com>
2022-12-18 12:23:33 +0100

    cleaning

--- server/database/adapter_mongoDB.ts
+++ server/database/adapter_mongoDB.ts
@@ -11,7 +11,7 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "development") {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     if (!(global as any)._mongoClientPromise) {
